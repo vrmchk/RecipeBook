2a11daf1224c3083733522b54c370328c6e77dc5
vrmchk <ihorveremchuk8@gmail.com>
2022-08-17 03:58:27 +0300

    User auth validation changed

--- Backend/RecipeBook/RecipeBook.BLL/Models/Auth/UserLoginRequest.cs
+++ Backend/RecipeBook/RecipeBook.BLL/Models/Auth/UserLoginRequest.cs
@@ -7,7 +7,6 @@ public class UserLoginRequest
     [Required, EmailAddress]
     public string Email { get; set; } = string.Empty;
 
-    //add regular expression
-    [Required, MinLength(8)]
+    [Required, StringLength(15, MinimumLength = 8)]
     public string Password { get; set; } = string.Empty;
 }
--- Backend/RecipeBook/RecipeBook.BLL/Models/Auth/UserRegisterRequest.cs
+++ Backend/RecipeBook/RecipeBook.BLL/Models/Auth/UserRegisterRequest.cs
@@ -6,8 +6,8 @@ public class UserRegisterRequest
 {
     [Required, EmailAddress]
     public string Email { get; set; } = string.Empty;
-    
-    //add regular expression
-    [Required, MinLength(8)]
+
+    [Required, StringLength(15, MinimumLength = 8)]
     public string Password { get; set; } = string.Empty;
+    
 }
