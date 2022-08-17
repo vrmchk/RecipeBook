33b19a0cb8e4f089708648136b5c02eea0a5832c
vrmchk <ihorveremchuk8@gmail.com>
2022-08-18 02:35:30 +0300

    password regex validation added

--- Backend/RecipeBook/RecipeBook.BLL/Models/Auth/UserRegisterRequest.cs
+++ Backend/RecipeBook/RecipeBook.BLL/Models/Auth/UserRegisterRequest.cs
@@ -8,5 +8,6 @@ public class UserRegisterRequest
     public string Email { get; set; } = string.Empty;
 
     [Required, StringLength(15, MinimumLength = 8)]
+    [RegularExpression(@"^(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%*#?&\.,:;])[A-Za-z\d@$!%*#?&\.,:;]+$")]
     public string Password { get; set; } = string.Empty;
 }
