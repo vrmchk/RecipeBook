9616dbd28f61bc64fab6a1b00fb7ecdba4999183
vrmchk <ihorveremchuk8@gmail.com>
2022-08-17 03:10:13 +0300

    ApplicationContext changed

--- Backend/RecipeBook/RecipeBook.DAL/Context/ApplicationContext.cs
+++ Backend/RecipeBook/RecipeBook.DAL/Context/ApplicationContext.cs
@@ -11,4 +11,13 @@ public class ApplicationContext : DbContext
     }
 
     public virtual DbSet<Recipe> Recipes { get; set; } = null!;
+    public virtual DbSet<Ingredient> Ingredients { get; set; } = null!;
+    public virtual DbSet<User> Users { get; set; } = null!;
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<User>()
+            .HasMany(user => user.Recipes)
+            .WithMany(recipe => recipe.UsersNavigation);
+    }
 }
