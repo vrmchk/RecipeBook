6b1f77d82344a2490446854b52d95ac980c9a042
vrmchk <ihorveremchuk8@gmail.com>
2022-08-17 23:38:26 +0300

    Custom exceptions and exception filter added

---
+++ Backend/RecipeBook/RecipeBook.BLL/Exceptions/RecipeDoesntExistException.cs
@@ -0,0 +1,9 @@
+﻿using RecipeBook.BLL.Exceptions.Base;
+
+namespace RecipeBook.BLL.Exceptions;
+
+public class RecipeDoesntExistException : CustomExceptionBase
+{
+    public RecipeDoesntExistException(string? cause, string message, Exception? innerException = null)
+        : base(cause, message, innerException) { }
+}
---
+++ Backend/RecipeBook/RecipeBook.BLL/Exceptions/UserDoesntExistException.cs
@@ -0,0 +1,9 @@
+﻿using RecipeBook.BLL.Exceptions.Base;
+
+namespace RecipeBook.BLL.Exceptions;
+
+public class UserDoesntExistException : CustomExceptionBase
+{
+    public UserDoesntExistException(string? cause, string message, Exception? innerException = null)
+        : base(cause, message, innerException) { }
+}
